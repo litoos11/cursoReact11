047c1938c6e7e929426fea224c836558bfee0009
litoos11 <litoos11@outlook.com>
2024-12-05 14:23:19 -0600

    feat: add useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { type User } from "./types.d";
 import "./App.css";
 import { UsersList } from "./components/UsersList";
@@ -7,6 +7,7 @@ function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
   const [sortByCountry, setSortByCountry] = useState(false);
+  const [filterCountry, setFilterCountry] = useState<string | null>(null);
   const originalUsers = useRef<User[]>([]);
 
   const toggleColors = () => {
@@ -38,23 +39,39 @@ function App() {
       });
   }, []);
 
-  const sortedUsers = sortByCountry
-    ? users.toSorted((a, b) => {
-        return a.location.country.localeCompare(b.location.country);
-      })
-    : users;
+  const filteredUsers = useMemo(() => {
+    return filterCountry != null && filterCountry.length > 0
+      ? users.filter((user) => {
+          return user.location.country
+            .toLowerCase()
+            .includes(filterCountry.toLowerCase());
+        })
+      : users;
+  }, [users, filterCountry]);
+
+  const sortedUsers = useMemo(() => {
+    return sortByCountry
+      ? filteredUsers.toSorted((a, b) => {
+          return a.location.country.localeCompare(b.location.country);
+        })
+      : filteredUsers;
+  }, [filteredUsers, sortByCountry]);
 
   return (
     <div className="App">
       <h1>Prueba técnica</h1>
       <header>
         <button onClick={toggleColors}>Colorear filas</button>
-
         <button onClick={toggleSortByCountry}>
           {sortByCountry ? "No ordenar por país" : "Ordenar por país"}
         </button>
-
         <button onClick={handleReset}>Resetea estado</button>
+        <input
+          placeholder="Filtra por país"
+          onChange={(e) => {
+            setFilterCountry(e.target.value);
+          }}
+        />
       </header>
       <main>
         <UsersList
